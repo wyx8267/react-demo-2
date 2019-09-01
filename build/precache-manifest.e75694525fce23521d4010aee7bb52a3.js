60bfea0094240ecc6a6dac37beca4f0453bd3ef8
wyx8267 <369264448@qq.com>
2019-09-01 11:39:55 +0800

    build

---
+++ build/precache-manifest.e75694525fce23521d4010aee7bb52a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375c4ce69a7eea8b68581f98a14888b8",
+    "url": "/react-demo-2/build/index.html"
+  },
+  {
+    "revision": "0b1d400f708fde759b42",
+    "url": "/react-demo-2/build/static/css/main.e3875cae.chunk.css"
+  },
+  {
+    "revision": "05675a673581296d13c0",
+    "url": "/react-demo-2/build/static/js/2.678a4eba.chunk.js"
+  },
+  {
+    "revision": "0b1d400f708fde759b42",
+    "url": "/react-demo-2/build/static/js/main.aabf28b5.chunk.js"
+  },
+  {
+    "revision": "bf21c1e16fb164f8109f",
+    "url": "/react-demo-2/build/static/js/runtime~main.d146e0de.js"
+  }
+]);
